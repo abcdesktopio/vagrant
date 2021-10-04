19a680d7db4716310288a7eeadf124162f3e9cec
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2021-10-04 14:44:46 +0200

    add comment

--- abcdesktop_base.sh
+++ abcdesktop_base.sh
@@ -8,8 +8,8 @@ echo 'install docker'
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo bash ./get-docker.sh
 # 
-# install docker compose
-echo 'install docker compose'
+# install docker-compose
+echo 'install docker-compose command'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # 
